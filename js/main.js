7abf013ac3ea74ade289cee4e3ce663ba9622881
Eduardo Moreira <eduardomoreiraemail96@gmail.com>
2019-08-23 08:48:38 -0300

    Iniciado as functions de conversão

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var celsius;
+var kelvin;
+var fahrenheit;
+
+function CelsiusToFehrenheit (celsius) 
+{
+    fahrenheit = parseFloat((celsius * 9/5) + 32);
+    return fahrenheit;
+}
+
+function CelsiusToKelvin (celsius) 
+{
+    kelvin = parseFloat(celsius + 273.15);
+    return kelvin;
+}
+
+function KelvinToCelsius (kelvin) 
+{
+    celsius = parseFloat(kelvin - 273.15);
+    return celsius;
+}
+
+function KelvinToFahrenheit (kelvin)
+{
+    fahrenheit = parseFloat(kelvin - 273.15);
+    return fahrenheit;
+}
+
+function FahrenheitToCelsius (fahrenheit) 
+{
+    celsius = parseFloat((fahrenheit - 32) * 5/9);
+    return celsius; 
+}
+
+function FahrenheitToKelvin (fahrenheit)
+{
+    kelvin = parseFloat((fahrenheit - 32) * 5/9 + 273.15);
+    return kelvin;
+
+    
+}
+
