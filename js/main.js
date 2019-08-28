d964b70d5e58e2bb8e536eb1e01b19eca04de34a
reinaldo ramos <reinaldo_ramos@estudante.sc.senai.br>
2019-08-28 11:55:17 -0300

    criando variáveis que puxam os valores do input addTemp

--- js/main.js
+++ js/main.js
@@ -1,43 +1,47 @@
 var celsius;
-var kelvin;
-var fahrenheit;
 
-function CelsiusToFehrenheit (celsius) 
-{
-    fahrenheit = parseFloat((celsius * 9/5) + 32);
+var inputELement = document.querySelector("#addTemp ");
+var select
+
+
+function CelsiusToFehrenheit(celsius) {
+    fahrenheit = parseFloat((celsius * 9 / 5) + 32);
     return fahrenheit;
 }
 
-function CelsiusToKelvin (celsius) 
-{
+function CelsiusToKelvin(celsius) {
     kelvin = parseFloat(celsius + 273.15);
     return kelvin;
 }
 
-function KelvinToCelsius (kelvin) 
-{
+function KelvinToCelsius(kelvin) {
     celsius = parseFloat(kelvin - 273.15);
     return celsius;
 }
 
-function KelvinToFahrenheit (kelvin)
-{
+function KelvinToFahrenheit(kelvin) {
     fahrenheit = parseFloat(kelvin - 273.15);
     return fahrenheit;
 }
 
-function FahrenheitToCelsius (fahrenheit) 
-{
-    celsius = parseFloat((fahrenheit - 32) * 5/9);
-    return celsius; 
+function FahrenheitToCelsius(fahrenheit) {
+    celsius = parseFloat((fahrenheit - 32) * 5 / 9);
+    return celsius;
 }
 
-function FahrenheitToKelvin (fahrenheit)
-{
-    kelvin = parseFloat((fahrenheit - 32) * 5/9 + 273.15);
+function FahrenheitToKelvin(fahrenheit) {
+    kelvin = parseFloat((fahrenheit - 32) * 5 / 9 + 273.15);
     return kelvin;
 
-    
-}
-console.log(celsius);
 
+}
+
+
+
+
+//lembrete: criar uma variável apenas que puxe o valor do input do "addTempo"
+//criar variáveis que puxem o valor dos selects
+//criar uma variável que puxe o valor do input do resultado, e através dele, aplique as functions de conversao
+//esboço: if(var input addtemp = tal and select.value temp = tal){
+
+//  function();}
