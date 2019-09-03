744561839631f250e2bd93bd79af8d2da17ac3da
Reinaldo <reinaldo_ramos@estudante.sc.senai.br>
2019-09-02 21:23:49 -0300

    terminando a função para realizar os cálculos, comentando e começando a criar as impossibilitações  nos ifs e elses

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 var selecionado;
 var convert;
 var inputElement;
 var valorResultado;
 var numero;
+
 function Selecionar() {
     selecionado = document.getElementById('AddTempValues').value;
     console.log(selecionado);
@@ -33,22 +33,84 @@ function Converter() {
 function RealizarCalculo() {
     valorResultado = document.getElementById('Resultado');
     if (selecionado == "C" && convert == "Fahrenheit") {
-        //  console.log(document.getElementById('addTemp').value)
-        calculo = parseFloat((document.getElementById('addTemp').value * 9 / 5) + 32);
-        //console.log(inputElement)   
+        calculo = parseFloat((document.getElementById('addTemp').value * 9 / 5) 
+        + 32);
         valorResultado.value = calculo;
         console.log(calculo);
-    }
-    else {
-        if () {
-            
-        } 
-        else {
-            
+        //Celsius para Fahrenheit
+        if (calculo < -479.67) {
+            valorResultado.value = null;
+            alert("Não é possível converter." +
+                " Razão: Temperatura em fahrenheit é menor que -479,67")
+
+
         }
 
     }
+    else {
+        if (selecionado == "C" && convert == "Kelvin") {
+            numero = parseFloat(document.getElementById('addTemp').value)
+            calculo = parseFloat(numero + 273.15);
+            valorResultado.value = calculo;
+            console.log(calculo);
+            //Celsius para Kelvin
+        }
+        else {
+            if (selecionado == "F" && convert == "Celsius") {
+                numero = parseFloat(document.getElementById('addTemp').value)
+                calculo = parseFloat((numero - 32) * 5 / 9);
+                valorResultado.value = calculo;
+                console.log(calculo);
+                //fahrenheit para Celsius
+            }
+            else {
+                if (selecionado == "F" && convert == "Kelvin") {
+                    numero = parseFloat(document.getElementById('addTemp').value)
+                    calculo = parseFloat((numero - 32) * 5 / 9 + 273.15);
+                    valorResultado.value = calculo;
+                    console.log(calculo);
+                    //Fahrenheit pra Kelvin
+                }
+                else {
+                    if (selecionado == "F" && convert == "Kelvin") {
+                        numero = parseFloat(document.getElementById('addTemp').value)
+                        calculo = parseFloat((numero - 32) * 5 / 9 + 273.15);
+                        valorResultado.value = calculo;
+                        console.log(calculo);
+                        //Fahrenheit para Kelvin
+                    }
+                    else {
+                        if (selecionado == "K" && convert == "Celsius") {
+                            numero = parseFloat(document.getElementById('addTemp').value)
+                            calculo = parseFloat(numero - 273.15);
+                            valorResultado.value = calculo;
+                            console.log(calculo);
+                            //Kelvin para Celsius
+                        }
+
+                        else {
+                            if (selecionado == "K" && convert == "Fahrenheit") {
+                                numero = parseFloat(document.getElementById('addTemp').value)
+                                calculo = parseFloat(1.8 * (numero - 273.15) + 32);
+                                valorResultado.value = calculo;
+                                console.log(calculo);
+                                //kelvin para fahrenheit
+                                if (calculo < -479.67) {
+                                    valorResultado.value = null;
+                                    alert("Não é possível converter." +
+                                        " Razão: Temperatura em fahrenheit é menor que -479,67")
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
 
 
 
@@ -57,127 +119,29 @@ function RealizarCalculo() {
 /*
 
 
-/*
-Selecionar();
-Temperatura();
-Converter();
-*/
-
-
-
-/*
-function CelsiusToFehrenheit() {
-    fahrenheit = parseFloat((addTemp * 9 / 5) + 32);
-=======
-var celsius;
-
-var inputELement = document.querySelector("#addTemp ");
-var selectElement = document.querySelector("#AddTempValues")
-var selectElementValue = document.querySelector("TempC")
-console.log(selectElementValue)
-
-
-
-
-
-function CelsiusToFehrenheit(celsius) {
-    fahrenheit = parseFloat((celsius * 9 / 5) + 32);
->>>>>>> 42ca0456c3246f2dd9c07f4f05852ae3ffddbc20
-    return fahrenheit;
-
-}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 42ca0456c3246f2dd9c07f4f05852ae3ffddbc20
-function CelsiusToKelvin(celsius) {
-    kelvin = parseFloat(celsius + 273.15);
-    return kelvin;
-}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 42ca0456c3246f2dd9c07f4f05852ae3ffddbc20
 function KelvinToCelsius(kelvin) {
     celsius = parseFloat(kelvin - 273.15);
     return celsius;
 }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 42ca0456c3246f2dd9c07f4f05852ae3ffddbc20
 function KelvinToFahrenheit(kelvin) {
     fahrenheit = parseFloat(kelvin - 273.15);
     return fahrenheit;
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 42ca0456c3246f2dd9c07f4f05852ae3ffddbc20
-function FahrenheitToCelsius(fahrenheit) {
-    celsius = parseFloat((fahrenheit - 32) * 5 / 9);
-    return celsius;
-}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 42ca0456c3246f2dd9c07f4f05852ae3ffddbc20
-function FahrenheitToKelvin(fahrenheit) {
-    kelvin = parseFloat((fahrenheit - 32) * 5 / 9 + 273.15);
-    return kelvin;
-
-
-<<<<<<< HEAD
-
 
 }
+
+
+
+
+
+
 
 */
-//lembrete: criar uma variável apenas que puxe o valor do input do "addTempo"	   
-//criar variáveis que puxem o valor dos selects	   
-//criar uma variável que puxe o valor do input do resultado, e através dele, aplique as functions de conversao	   
-//esboço: if(var input addtemp = tal and select.value temp = tal){	   
-
-
-//  function();}	 
-
-/*
-    var ValorResultado = document.getElementById('Resultado').value;
-    document.getElementById('receber_resultado').value = ValorResultado;
-*/
-/*
-function RealizarCalculo(receber_valor, receber_temp, receber_convert, receber_resultado) {
-    var ValorResultado = document.getElementById('Resultado').value;
-    document.getElementById('receber_resultado').value = ValorResultado;
-    if (receber_valor == "C" && receber_convert == "Fahrenheit") {
-        var Calculo = parseFloat((receber_temp * 9 / 5) + 32);
-        receber_resultado.value = Calculo;
-        console.log(receber_resultado);
-
-
-
-    }
-
-
-
-}
-*/
-=======
-}
 
 
 
 
 
-//lembrete: criar uma variável apenas que puxe o valor do input do "addTempo"
-//criar variáveis que puxem o valor dos selects
-//criar uma variável que puxe o valor do input do resultado, e através dele, aplique as functions de conversao
-//esboço: if(var input addtemp = tal and select.value temp = tal){
-
-//  function();}
->>>>>>> 42ca0456c3246f2dd9c07f4f05852ae3ffddbc20
